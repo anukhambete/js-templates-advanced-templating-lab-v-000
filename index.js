a2a379a71cda232bbfc27b1369648e29f04bf42f
Anu Khambete <anukhambete@gmail.com>
2018-12-01 21:44:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,62 @@
-function init() {
-  //put any page initialization/handlebars initialization here
-  let recipe = {
-      description: 'yummy chicken noodle soup',
-      ingredients: [
-        {quantity: "1 cup", name: 'chicken'},
-        {quantity: "3 nanoliters", name: 'stock'},
-        {quantity: "12", name: 'noodles'}
-      ]
-    }
+Handlebars.registerHelper('displayIngredient', function() {
+  if (this != "") {
+    //debugger
+     var result = '<li name="ingredients">' + this.text + '</li>';
+    return new Handlebars.SafeString(result);
+  }
+})
 
-    let template = document.getElementById("my-template").innerHTML;
-    let templateFn = Handlebars.compile(template);
-    let html = templateFn(recipe);
-    document.getElementsByTagName("main")[0].innerHTML += html;
+function init() {
+  let ingredientsValues = [];
+  var ingr = document.getElementsByName("ingredients");
+  for(i=0;i<ingr.length;i++){
+  if(ingr[i].value != "") {
+  ingredientsValues.push(ingr[i].value)
+  }
+  }
+  //put any page initialization/handlebars initialization here
+  // let recipe = {
+  //     description: 'yummy chicken noodle soup',
+  //     ingredients: [
+  //       {quantity: "1 cup", name: 'chicken'},
+  //       {quantity: "3 nanoliters", name: 'stock'},
+  //       {quantity: "12", name: 'noodles'}
+  //     ]
+  //   }
+  let template = document.getElementById("recipe-form-template").innerHTML;
+  let templateFn = Handlebars.compile(template);
+  let html = templateFn();
+  document.getElementsByTagName("main")[0].innerHTML += html;
+
+
+    // Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+    // function renderMain() {
+    //   let template = document.getElementById("recipe-template").innerHTML;
+    //   let templateFunction = Handlebars.compile(template);
+    //   let html = templateFunction({});
+    //   }
+
+
+
+
+
 }
+
+
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+
+// <script id="my-template" type="text/x-handlebars-template">
+//   <div id="recipe">
+//     <p>{{description}}</p>
+//     <ul>
+//       {{#each ingredients}}
+//       <li>{{name}}</li>
+//       {{/each}}
+//     </ul>
+//   </div>
+// </script>
